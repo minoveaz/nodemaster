0c063a50180ff41d862338ff4a71f88d469b375a
Miller Camilo Vega Diaz <ing.miller.vega@gmail.com>
2021-01-04 13:54:19 +0100

    restserver actions

--- 07-restserver/server/server.js
+++ 07-restserver/server/server.js
@@ -1,23 +1,45 @@
+require('./config/config')
+
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-parser')
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.get('/user', (req, res) => {
     res.send('Get Usuario')
 })
 
 app.post('/user', (req, res) => {
-    res.send('Post Usuario')
+    let body = req.body;
+    if (body.name === undefined) {
+        res.status(400).json({
+            ok: false,
+            message: 'name is required',
+        })
+    } else {
+        res.json({
+            persona: body
+        });
+    }
 })
 
-app.put('/user', (req, res) => {
-    res.send('Put Usuario')
+app.put('/user/:id', (req, res) => {
+    let id = req.params.id;
+    res.json({
+        id
+    });
 })
 
 app.delete('/user', (req, res) => {
     res.send('Delete Usuario')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening at http://localhost:${process.env.PORT}`)
 })
